1ed038cd2e53e35142f7edaadd10cb0fdc1ef80a
Laura Richer <user@macbook-pro.home>
2022-06-15 15:11:35 +0100

    adding typescript

--- src/App.vue
+++ src/App.vue
@@ -1,74 +1,27 @@
-<script setup lang="ts">
-import CalculateData from './components/CalculateData.vue'
-import CalculateTime from './components/CalculateTime.vue'
+<script lang="ts">
+import CalculateData from './components/CalculateData.vue';
+import CalculateTime from './components/CalculateTime.vue';
+
+export default {
+  setup() {
+    const handleClick = (calculatorType: string) => {
+      console.log(calculatorType);
+    }
+  }
+};
 </script>
 
 <template>
   <main>
+    <div>
+      <span @click="handleClick(1)">Calculate data</span>
+      <span @click="handleClick('time')">Calculate time</span>
+    </div>
     <CalculateData />
     <CalculateTime />
   </main>
 </template>
 
-<style>
-@import './assets/base.css';
-
-#app {
-  max-width: 1280px;
-  margin: 0 auto;
-  padding: 2rem;
-
-  font-weight: normal;
-}
-
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-a,
-.green {
-  text-decoration: none;
-  color: hsla(160, 100%, 37%, 1);
-  transition: 0.4s;
-}
-
-@media (hover: hover) {
-  a:hover {
-    background-color: hsla(160, 100%, 37%, 0.2);
-  }
-}
-
-@media (min-width: 1024px) {
-  body {
-    display: flex;
-    place-items: center;
-  }
-
-  #app {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    padding: 0 2rem;
-  }
-
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-}
+<style lang="scss">
+@import './scss/app.scss';
 </style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { fileURLToPath, URL } from 'url'
 
 import { defineConfig } from 'vite'
+import checker from 'vite-plugin-checker'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), checker({ typescript: true })],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
